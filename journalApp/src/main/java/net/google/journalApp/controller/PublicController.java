05ee186564e775e77f4042ed8d1192fa2fb19841
Shubham Chikane <123558983+ShubhamChikane99@users.noreply.github.com>
2024-09-15 16:21:59 +0530

    //shubham changes

---
+++ journalApp/src/main/java/net/google/journalApp/controller/PublicController.java
@@ -0,0 +1,32 @@
+package net.google.journalApp.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import net.google.journalApp.entity.Users;
+import net.google.journalApp.exception.ServiceResponse;
+import net.google.journalApp.service.UsersService;
+
+@RestController
+@RequestMapping("/public")
+public class PublicController {
+
+	@Autowired
+	private UsersService usersService;
+
+	@PostMapping
+	public ServiceResponse saveUsers(@RequestBody Users users) {
+
+		return ServiceResponse.asSuccess(usersService.saveUsers(users));
+	}
+
+	@GetMapping("health-check")
+	public String healthCheck() {
+
+		return "OK";
+	}
+}
