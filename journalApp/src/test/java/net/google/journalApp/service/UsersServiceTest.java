66b02c1681e53784ae9e8de2d352f87e734aa471
Shubham Chikane <123558983+ShubhamChikane99@users.noreply.github.com>
2024-09-28 22:27:43 +0530

    //shubham changes

--- journalApp/src/test/java/net/google/journalApp/service/UsersServiceTest.java
+++ journalApp/src/test/java/net/google/journalApp/service/UsersServiceTest.java
@@ -2,11 +2,13 @@ package net.google.journalApp.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import net.google.journalApp.entity.Users;
 import net.google.journalApp.repository.UsersRepository;
 
 @SpringBootTest
@@ -19,6 +21,12 @@ public class UsersServiceTest {
 	public void testFindByUserName() {
 		  assertEquals(4, 2 + 2); 
 		  
-		 assertNotNull(usersRepository.findUserByUserName("ajay_zalte"));
+		  assertNotNull(usersRepository.findUserByUserName("ajay_zalte"));
+		  
+		  Users user = usersRepository.findUserByUserName("ajay_zalte");
+		  
+		  assertTrue(user.getMobileNo().isEmpty());
+		  
+		
 	}
 }
