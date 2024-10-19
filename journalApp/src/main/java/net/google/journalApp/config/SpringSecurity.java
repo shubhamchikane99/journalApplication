d5adb504133a541412e98fa4d89270d98dd54669
Shubham Chikane <123558983+ShubhamChikane99@users.noreply.github.com>
2024-10-19 21:29:21 +0530

    //shubham changes

--- journalApp/src/main/java/net/google/journalApp/config/SpringSecurity.java
+++ journalApp/src/main/java/net/google/journalApp/config/SpringSecurity.java
@@ -3,6 +3,7 @@ package net.google.journalApp.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -15,6 +16,7 @@ import net.google.journalApp.service.UsersDetailsServiceImpl;
 
 @Configuration
 @EnableWebSecurity
+@Profile("prod")
 public class SpringSecurity extends WebSecurityConfigurerAdapter {
 
 	@Autowired
@@ -26,7 +28,7 @@ public class SpringSecurity extends WebSecurityConfigurerAdapter {
 		http.authorizeRequests()
 		.antMatchers("/v1/**").authenticated()
 		.antMatchers("/admin/**").hasRole("ADMIN")
-		.anyRequest().permitAll() 
+		.anyRequest().authenticated()
 		.and()
 		.httpBasic();
 		 http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().csrf().disable();
