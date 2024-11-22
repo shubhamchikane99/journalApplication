f5334e15fee6e7beb1167ebedd4aaad464183fa8
Legend <Legend@DESKTOP-EN00QNP>
2024-11-22 23:29:38 +0530

    //shubham changes

---
+++ journalApp/src/main/java/net/google/journalApp/controller/ThirdPartyApiIntegrationController.java
@@ -0,0 +1,25 @@
+package net.google.journalApp.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import net.google.journalApp.exception.ResourceNotFoundException;
+import net.google.journalApp.exception.ServiceResponse;
+import net.google.journalApp.service.ThirdPartyApiIntegrationService;
+
+@RestController
+@RequestMapping("v1/api-integration")
+public class ThirdPartyApiIntegrationController {
+
+	@Autowired
+	private ThirdPartyApiIntegrationService thirdPartyApiIntegrationService;
+
+//	@GetMapping("/weather-api")
+//	public ServiceResponse weatherApiIntegration(@RequestParam("cityName") String cityName)
+//			throws ResourceNotFoundException {
+//		return ServiceResponse.asSuccess(thirdPartyApiIntegrationService.weatherApiIntegration(cityName));
+//	}
+}
