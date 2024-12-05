6abf97834900d920c8b1aeb0f9bf39bc0c0864c5
shubhamchikane99 <shubhamchikane41216@gmail.com>
2024-12-05 21:23:17 +0530

    //shubham changes

---
+++ journalApp/src/main/java/net/google/journalApp/service/ConfigJournalAppService.java
@@ -0,0 +1,56 @@
+package net.google.journalApp.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import net.google.journalApp.entity.ConfigJournalApp;
+import net.google.journalApp.entity.ErrorMessage;
+import net.google.journalApp.repository.ConfigJournalAppRepository;
+
+@Service
+public class ConfigJournalAppService {
+
+	@Autowired
+	private ConfigJournalAppRepository configJournalAppRepository;
+
+	public ConfigJournalApp saveConfigJournalApp(ConfigJournalApp configJournalApp) {
+		// Save Journal Entry
+
+		return configJournalAppRepository.save(configJournalApp);
+	}
+
+	public ConfigJournalApp configJournalAppById(String id) {
+		// Journal Entry By Id
+
+		Optional<ConfigJournalApp> configJournalAppOpt = configJournalAppRepository.findById(id);
+
+		ConfigJournalApp configJournalApp = configJournalAppOpt
+				.orElseThrow(() -> new RuntimeException("ConfigJournalApp not found for id: " + id));
+
+		return configJournalApp;
+	}
+
+	public ErrorMessage deleteConfigJournalAppById(String id) {
+		// Journal Entry Delete By Id
+
+		ErrorMessage errorMessage = new ErrorMessage();
+
+		errorMessage.setError(true);
+		errorMessage.setStatusCode(500);
+		errorMessage.setErrorMessage("failed to delete.");
+
+		int result = configJournalAppRepository.deleteConfigJournalAppById(id);
+
+		if (result > 0) {
+
+			errorMessage.setError(false);
+			errorMessage.setStatusCode(200);
+			errorMessage.setErrorMessage("Delete Successfully.");
+		}
+
+		return errorMessage;
+	}
+
+}
