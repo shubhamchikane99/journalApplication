3beb9cdd2eaa76a0a350fb670148741ace604879
shubhamchikane99 <shubhamchikane41216@gmail.com>
2024-12-21 22:55:46 +0530

    //shubham changes for redis

--- journalApp/src/main/java/net/google/journalApp/service/RedisService.java
+++ journalApp/src/main/java/net/google/journalApp/service/RedisService.java
@@ -1,8 +1,15 @@
 package net.google.journalApp.service;
 
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Service
 public class RedisService {
@@ -13,16 +20,30 @@ public class RedisService {
 	public String redis() {
 		// This API For Test Redis DB
 
-		
 		redisTemplate.opsForValue().set("email", "shubhamchikane41216@gmail.com");
 
-		Object email = redisTemplate.opsForValue().get("salary"); 
-
-		//System.err.println("email " + email);
-
+		Object email = redisTemplate.opsForValue().get("salary");
 		String eml = email.toString();
 
 		return eml;
+	}
+
+	public <T> T get(String key, Class<T> entityClass) throws JsonMappingException, JsonProcessingException {
+		// Get Key From Redis
+
+		Object obj = redisTemplate.opsForValue().get(key);
+		ObjectMapper mapper = new ObjectMapper();
+		if (!Objects.isNull(obj)) {
+			return mapper.readValue(obj.toString(), entityClass);
+		} else {
+			return null;
+		}
+	}
+
+	public void set(String key, Object obj, Long ttl) throws JsonMappingException, JsonProcessingException {
+		// Get Key From Redis
+
+		redisTemplate.opsForValue().set(key, obj.toString(), ttl, TimeUnit.SECONDS);
 	}
 
 }
--- journalApp/src/main/java/net/google/journalApp/service/ThirdPartyApiIntegrationService.java
+++ journalApp/src/main/java/net/google/journalApp/service/ThirdPartyApiIntegrationService.java
@@ -1,5 +1,7 @@
 package net.google.journalApp.service;
 
+import java.util.Objects;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpMethod;
@@ -7,35 +9,59 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+
 import net.google.journalApp.cache.AppCache;
 import net.google.journalApp.entity.Weather;
 
 @Service
 public class ThirdPartyApiIntegrationService {
 
-	
 	@Value("${weather_key}")
 	private String weatherKey;
-	
 
-	//private static  String API = "http://api.weatherstack.com/current?access_key=weatherKey&query=city";
+	// private static String API =
+	// "http://api.weatherstack.com/current?access_key=weatherKey&query=city";
 
 	@Autowired
-	private RestTemplate restTemplate; 
+	private RestTemplate restTemplate;
 
 	@Autowired
 	private AppCache appCache;
-	
-	public Weather weatherApiIntegration(String cityName) {
+
+	@Autowired
+	private RedisService redisService;
+
+	public Weather weatherApiIntegration(String cityName) throws JsonMappingException, JsonProcessingException {
 		// Weather API Integration
 
-		String finalAPI = appCache.APP_CACHE.get("weather_api").replace("<city>", cityName).replace("<weatherKey>", weatherKey);
+		Weather weatherRes = new Weather();
+		try {
+			Weather weather = redisService.get("weather_of_" + cityName, Weather.class);
 
-		ResponseEntity<Weather> reponse = restTemplate.exchange(finalAPI, HttpMethod.GET, null, Weather.class);
-		Weather weatherRes = reponse.getBody();
+			if (!Objects.isNull(weather)) {
 
-		System.err.println("weatherRes " + weatherRes);
-		return weatherRes; 
+				return weather;
+
+			} else {
+
+				String finalAPI = appCache.APP_CACHE.get("weather_api").replace("<city>", cityName)
+						.replace("<weatherKey>", weatherKey);
+				ResponseEntity<Weather> reponse = restTemplate.exchange(finalAPI, HttpMethod.GET, null, Weather.class);
+				weatherRes = reponse.getBody();
+				System.err.println("weatherRes " + weatherRes);
+
+				if (!Objects.isNull(weatherRes)) {
+
+					redisService.set("weather_of_" + cityName, weatherRes, (long) 3001);
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return weatherRes;
 	}
 
 }
