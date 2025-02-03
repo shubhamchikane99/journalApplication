d081e1229e9c5182d22c17839547913e18ed7f8a
shubhamchikane99 <shubhamchikane41216@gmail.com>
2025-02-03 23:40:39 +0530

    //shubham changes

--- journalApp/src/main/java/net/google/journalApp/controller/ChatController.java
+++ journalApp/src/main/java/net/google/journalApp/controller/ChatController.java
@@ -1,21 +1,26 @@
 package net.google.journalApp.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import net.google.journalApp.entity.ChatMessage;
+import net.google.journalApp.repository.ChatMessageRepository;
 
 @Controller
 public class ChatController {
 
 	private final SimpMessagingTemplate messagingTemplate;
+
+	@Autowired
+	private ChatMessageRepository chatMessageRepository;
 
 	public ChatController(SimpMessagingTemplate messagingTemplate) {
 		this.messagingTemplate = messagingTemplate;
@@ -23,12 +28,23 @@ public class ChatController {
 
 	@MessageMapping("/private-message")
 	public void sendPrivateMessage(@Payload ChatMessage chatMessage) {
-	    System.out.println("📩 Message from: " + chatMessage.getSenderId());
-	    System.out.println("📩 Message to: " + chatMessage.getReceiverId());
-	    System.out.println("📩 Content: " + chatMessage.getContent());
+		System.out.println("📩 Message from: " + chatMessage.getSenderId());
+		System.out.println("📩 Message to: " + chatMessage.getReceiverId());
+		System.out.println("📩 Content: " + chatMessage.getContent());
 
-	    // Ensure messages are sent to the correct user destination
-	    messagingTemplate.convertAndSendToUser(chatMessage.getReceiverId(), "/private", chatMessage);
+		chatMessageRepository.save(chatMessage);
+
+		// Ensure messages are sent to the correct user destination
+		messagingTemplate.convertAndSendToUser(chatMessage.getReceiverId(), "/private", chatMessage);
+	}
+
+	// Fetch chat history between two users
+	@GetMapping("/messages/{senderId}/{receiverId}")
+	public ResponseEntity<List<ChatMessage>> getMessages(@PathVariable String senderId,
+			@PathVariable String receiverId) {
+		List<ChatMessage> messages = chatMessageRepository
+				.findBySenderIdAndReceiverIdOrReceiverIdAndSenderIdOrderByTimestamp(senderId, receiverId);
+		return ResponseEntity.ok(messages);
 	}
 
 }
