fed3a478d274f98a48587932d63e5d946bb4d323
Shubham Chikane <123558983+shubhamchikane99@users.noreply.github.com>
2025-04-05 16:57:48 +0530

    Update AppCache.java

--- journalApp/src/main/java/net/google/journalApp/cache/AppCache.java
+++ journalApp/src/main/java/net/google/journalApp/cache/AppCache.java
@@ -25,11 +25,11 @@ public class AppCache {
 
 		APP_CACHE = new HashMap<>();
 
-		List<ConfigJournalApp> all = new ArrayList<ConfigJournalApp>();
+		//List<ConfigJournalApp> all = new ArrayList<ConfigJournalApp>();
 			//configJournalAppRepository.getAllConfigJournalApp();
 
-		for (ConfigJournalApp obj : all) {
-		APP_CACHE.put(obj.getJournalKey(), obj.getJournalValue());
-		}
+		//for (ConfigJournalApp obj : all) {
+		//APP_CACHE.put(obj.getJournalKey(), obj.getJournalValue());
+		//}
 	}
 }
