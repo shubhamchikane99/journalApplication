3d1fad20413feb9ff6f5202f24de321cbe1f805e
Shubham Chikane <123558983+shubhamchikane99@users.noreply.github.com>
2025-05-15 23:42:51 +0530

    Update AccessRole.java

--- journalApp/src/main/java/net/google/journalApp/entity/AccessRole.java
+++ journalApp/src/main/java/net/google/journalApp/entity/AccessRole.java
@@ -4,7 +4,7 @@ import java.util.Date;
 import java.util.UUID;
 
 import javax.persistence.Column;
-import javax.persistence.Entity;
+import javax.persistence.Entity; 
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
