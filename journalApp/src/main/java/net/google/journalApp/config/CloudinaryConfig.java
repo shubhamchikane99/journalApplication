2f88fb03e56f3c58225d9b9f6667e8ff470c5753
Shubham Chikane <123558983+shubhamchikane99@users.noreply.github.com>
2025-05-19 23:19:54 +0530

    Update CloudinaryConfig.java

--- journalApp/src/main/java/net/google/journalApp/config/CloudinaryConfig.java
+++ journalApp/src/main/java/net/google/journalApp/config/CloudinaryConfig.java
@@ -1,6 +1,6 @@
 package net.google.journalApp.config;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Value; 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
