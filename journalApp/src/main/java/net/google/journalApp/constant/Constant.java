6880c572dcca866f85cf23a7372700974a43e206
Shubham Chikane <123558983+shubhamchikane99@users.noreply.github.com>
2025-07-06 15:41:28 +0530

    Update Constant.java

--- journalApp/src/main/java/net/google/journalApp/constant/Constant.java
+++ journalApp/src/main/java/net/google/journalApp/constant/Constant.java
@@ -2,13 +2,12 @@ package net.google.journalApp.constant;
 
 public class Constant {
 
-	private static final String weatherApiKey = "123";
+	private static final String weatherApiKey = "123"; 
 
 	private static final String API = "Requesthttp://api.weatherstack.com/current? access_key=weatherApiKey&query=CITY";
-
+  
 	// localhost
-	public static final String backendUrl = "http://localhost:8080/";
+	//public static final String backendUrl = "http://localhost:8080/";
 
-	// public static final String backendUrl =
-	// "https://journalapplication-production-05b9.up.railway.app/";
+	public static final String backendUrl = "https://journalapplication-production-4eb9.up.railway.app/";
 }
